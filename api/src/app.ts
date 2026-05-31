657bcc6edd66330d1f9a1486244cf3a3cdd01eb9
shashwotghimire <shashwot234@gmail.com>
2026-05-31 14:56:39 +0545

    fix: cors origin

--- api/src/app.ts
+++ api/src/app.ts
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://documentgpt.shashwotghimire.com.np",
+      "https://www.documentgpt.shashwotghimire.com.np",
     ],
     credentials: true,
   }),
