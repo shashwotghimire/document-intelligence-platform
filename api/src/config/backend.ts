3a096ae64ac6d695c2348403ba7e192cd6a373bf
shashwotghimire <shashwot234@gmail.com>
2026-06-03 21:22:40 +0545

    fix: oauth related links

--- api/src/config/backend.ts
+++ api/src/config/backend.ts
@@ -1,2 +1,2 @@
-export const backendOrigin = `http://localhost:8080`;
-export const backendOriginProduction = `https://shashwotghimire.tech`;
+// export const backendOrigin = `http://localhost:8080`;
+export const backendOrigin = `https://shashwotghimire.tech`;
